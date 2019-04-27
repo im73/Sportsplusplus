273dee691db4c3a0d1e3d59cbebb33b6bfa76ef0
MichaelJack_ <zhu73a@163.com>
2019-04-28 02:39:25 +0800

    Merge branch 'master' of https://github.com/im73/Sportsplusplus
                                                                           # Conflicts:
                                                                           #	.idea/workspace.xml
                                                                           增加用户统计api

--- auto_schedule.py
+++ auto_schedule.py
@@ -2,6 +2,6 @@ from data_spider import DataSpider
 from storedata import store_schedule,updatetoken
 
 updatetoken()
-# ds = DataSpider()
-# ds.get_team_schedule_hupu()
-# store_schedule()
+ds = DataSpider()
+ds.get_team_schedule_hupu()
+store_schedule()
