74231b8fcd9dd60a92a7e07fe5bf5cacbedb094a
MichaelJack_ <zhu73a@163.com>
2019-05-06 19:16:20 +0800

    Merge branch 'master' of https://github.com/im73/Sportsplusplus
             # Conflicts:
             #	.idea/workspace.xml
    
           未来比赛存储

--- auto_playing_game.py
+++ auto_playing_game.py
@@ -1,9 +1,9 @@
 from  data_spider import DataSpider
-from storedata import store_history_game,delete_files
+from storedata import store_history_game,store_playing,delete_files
 
 
 ds = DataSpider()
 
 ds.get_playing_game_info_hupu()
-store_history_game()
+store_playing()
 delete_files()
